387b3b5ef91cf0384b1fbfad7d477a1e2e8774b3
Tomasz Muras <nexor1984@gmail.com>
2023-02-23 19:47:01 +0100

    Handle the same symbols for different grades

--- resources/import-pistacja.php
+++ resources/import-pistacja.php
@@ -5,11 +5,17 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-$subject = 'biologia';
-$file = 'BIO-SP46.txt';
+$subject = 'matematyka';
+//$subject = 'fizyka';
+//$file = 'BIO-SP46.txt';
 //$file = 'BIO-SP78.txt';
+//$file = 'CHM-SP78.txt';
+//$file = 'FIZ-SP78.txt';
+$file = 'MAT-SP46.txt';
+//$file = 'MAT-SP78.txt';
+$grade7 = 0;
 
-$sql = "SELECT id FROM curriculum WHERE subject = ? AND symbol = ?";
+$sql = "SELECT id FROM curriculum WHERE subject = ? AND symbol = ? AND grade7 = ?";
 $preparedStatement = $dbh->prepare($sql);
 
 $resources = file($file);
@@ -25,7 +31,7 @@ foreach ($resources as $resource) {
     foreach ($split as $symbol) {
         $symbol .= '.';
         echo "$symbol\n";
-        $result = $preparedStatement->execute([$subject, $symbol]);
+        $result = $preparedStatement->execute([$subject, $symbol, $grade7]);
         $id = $preparedStatement->fetchAll();
         if (count($id) != 1) {
             var_dump($id);
